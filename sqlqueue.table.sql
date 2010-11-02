a8f9e1354fec3b4a748e60019b55c235207c6749
Zach Bonham <zachbonham@gmail.com>
2010-11-02 07:38:45 -0500

    removed dynamic sql; reemoved idea of schema per account;

--- sqlqueue.table.sql
+++ sqlqueue.table.sql
@@ -1,29 +1,20 @@
+USE queues;
 
-/****** Object:  Table [dbo].[MyQueue]    Script Date: 12/07/2009 07:49:28 ******/
-SET ANSI_NULLS ON
 GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-SET ANSI_PADDING ON
-GO
-
-CREATE TABLE [crm].[MyQueue](
+CREATE TABLE [QueueTable]
+(
 	[Id] [bigint] IDENTITY(1,1) NOT NULL,
-	[MessageId] [uniqueidentifier] NOT NULL,
-	[ReceiveId] [uniqueidentifier] NULL,
-	[Body] [varbinary](max) NULL,
-	[DequeCount] [int] default 0,
-	[Status] [char](10) NOT NULL,
-	[TimeToLive] [int] NOT NULL,
-	[TimeToReceive] [int] NULL,
-	[Inserted] [datetime] NOT NULL,
-	[Received] [datetime] NULL,
-	[Expires] [datetime] NOT NULL
+	[QueueId] [BIGINT] NOT NULL,
+	[MessageId] [UNIQUEIDENTIFIER] NOT NULL,
+	[ReceiveId] [UNIQUEIDENTIFIER] NULL,
+	[Body] [VARBINARY](MAX) NULL,
+	[DequeCount] [INT] default 0,
+	[Status] [CHAR](10) NOT NULL,
+	[TimeToLive] [INT] NOT NULL,
+	[TimeToReceive] [INT] NULL,
+	[Inserted] [DATETIME] NOT NULL,
+	[Received] [DATETIME] NULL,
+	[Expires] [DATETIME] NOT NULL
 ) ON [PRIMARY]
 
-GO
-
-SET ANSI_PADDING OFF
 GO
--- sqlqueue_putmessage.proc.sql
+++ sqlqueue_putmessage.proc.sql
@@ -1,29 +1,14 @@
-USE [MessageQueues]
-GO
-
 /****** Object:  StoredProcedure [dbo].[PutMessage]    Script Date: 12/22/2009 16:58:11 ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[PutMessage]') AND type in (N'P', N'PC'))
 DROP PROCEDURE [dbo].[PutMessage]
 GO
 
-USE [MessageQueues]
-GO
-
-/****** Object:  StoredProcedure [dbo].[PutMessage]    Script Date: 12/22/2009 16:58:11 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-
 CREATE PROCEDURE [dbo].[PutMessage]
-	@Schema			nvarchar(256),
-	@Queue			nvarchar(50),
-    @MessageID		uniqueidentifier,
-    @TimeToLive		int = 604800,
-    @Body			varbinary(max),
-	@Debug			int = 0
+    @QueueID		BIGINT,
+    @MessageID		UNIQUEIDENTIFIER,
+    @Body			VARBINARY(MAX),
+    @TimeToLive		INT = 604800,
+    @Debug			INT = 0
 AS
 BEGIN
     
@@ -51,46 +36,29 @@ BEGIN
 
 -- validate schema and queue exist before we get here
 --
-	SET @SqlInsert = N'
-		INSERT INTO [' + @Schema + '].[' + @Queue + ']
-		([Status],
-		 [MessageId],
-		 [TimeToLive],
-		 [Body],
-		 [Inserted],
-		 [Expires]
-		 )
-		VALUES
-		(''U'',
-		 @N_MessageId,
-		 @N_TimeToLive,
-		 @N_Body,
-		 @N_Inserted,
-		 @N_Expires
-		 )'
 
-	SET @SqlParams = N'
-		@N_MessageId uniqueidentifier,
-		@N_TimeToLive int,
-		@N_Body varbinary(max), 
-		@N_Inserted datetime,
-		@N_Expires datetime'
+INSERT INTO [DBO].[QUEUETABLE]
+(
+	[QueueID]
+	, [Status]
+	, [MessageID]
+	, [TimeToLive]
+	, [Body]
+	, [Inserted]
+	, [Expires]
+)
+VALUES
+(
 	
-	IF @Debug = 0
-		BEGIN
-			EXEC sp_executesql @SqlInsert, @SqlParams, 
-					@N_MessageId = @MessageId,
-					@N_TimeToLive = @TimeToLive,
-					@N_Body = @Body,
-					@N_Inserted = @Inserted,
-					@N_Expires = @Expires
-					
-		END
-	ELSE
-		BEGIN
-			PRINT @SqlInsert
-		END
-
+	@QueueID
+	, 'U'
+	, @MessageID
+	, @TimeToLive
+	, @Body
+	, @Inserted
+	, @Expires
+)
+	
 	 -- Check for errors and abort if found
 	SELECT @Error = @@ERROR
 	IF @Error <> 0 BEGIN
@@ -112,13 +80,6 @@ lbl_end:
     RETURN @Return
 
 END 
-
-
-
-
-
-
-
 
 
 GO
